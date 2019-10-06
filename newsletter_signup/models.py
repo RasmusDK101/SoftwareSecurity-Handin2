44f105826816ed9cb1cc736670bdb7490f69d37b
Rasmus <rasmus@tow.dk>
2019-10-06 21:42:24 +0200

    Added validators

--- newsletter_signup/models.py
+++ newsletter_signup/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.core import validators
 
 class NewsletterSignup(models.Model):
-    username = models.CharField(max_length = 100, validators=[validators.validate_slug])
-    firstnames = models.CharField(max_length = 100, validators=[validators.validate_slug])
-    lastnames = models.CharField(max_length = 100, validators=[validators.validate_slug])
-    email = models.EmailField(validators=[validators.validate_email])
+    username = models.CharField(max_length = 100, validators=[validators.validate_unicode_slug])
+    firstnames = models.CharField(max_length = 100, validators=[validators.validate_unicode_slug])
+    lastnames = models.CharField(max_length = 100, validators=[validators.validate_unicode_slug])
+    email = models.EmailField()
 
     def __str__(self):
             return self.email
