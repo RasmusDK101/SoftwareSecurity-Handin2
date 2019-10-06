2cb8c93603a564764e972e271ef2d73f913aadbe
Rasmus <rasmus@tow.dk>
2019-10-06 23:17:08 +0200

    Added verify script

---
+++ verifyNewsletterSignup.py
@@ -0,0 +1,16 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_unicode_slug
+from newsletter_signup.models import NewsletterSignup
+
+def validate_name(name):
+    try:
+        validate_unicode_slug(name)
+    except ValidationError:
+        return False
+    else:
+        return True 
+
+for obj in NewsletterSignup.objects.all():
+    if  not validate_name(obj.username) or not validate_name(obj.firstnames) or not validate_name(obj.lastnames):
+        print(obj.id)
+        NewsletterSignup.objects.filter(id=obj.id).delete()
